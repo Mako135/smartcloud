a4110288f94bc41926fdad96102f3440eaba9fca
Kuanyshbek.M <Kuanyshbek.M@kazteleport.kz>
2024-11-29 17:03:02 +0500

    Optimizing #3

--- src/components/shared/SecurityAccordion.tsx
+++ src/components/shared/SecurityAccordion.tsx
@@ -11,7 +11,7 @@ interface AccordionProps {
 
 export function SecurityAccordion({ data }: AccordionProps) {
   return (
-    <div className="my-60">
+    <section className="my-60">
       <h2 className="text-center text-4xl font-medium">Для кого подходит?</h2>
       <Accordion type="single" collapsible className="w-full mt-20">
         {data.map((item, index) => (
@@ -25,6 +25,6 @@ export function SecurityAccordion({ data }: AccordionProps) {
           </AccordionItem>
         ))}
       </Accordion>
-    </div>
+    </section>
   );
 }
